162f9006dbfa693df960e8cbf4fe71db854207b0
Disbotsxyz <87479238+Disbotsxyz@users.noreply.github.com>
2021-08-01 08:28:16 +0530

    Fixed makeid not defined

--- src/routers/admin/codeshare/add.js
+++ src/routers/admin/codeshare/add.js
@@ -42,5 +42,14 @@ app.post("/admin/addcode", global.checkAuth, async (req, res) => {
     )
     res.redirect('/code/' + kod)
 });
+function makeid(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
 
 module.exports = app;
